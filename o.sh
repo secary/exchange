b35bd991ebb62e93a03c07c81ae87af6a9704633
secary <secary2010@outlook.com>
2025-03-15 18:06:55 +1030

    added date in switch shells

--- o.sh
+++ o.sh
@@ -1,5 +1,5 @@
 # !/bin/zsh
 # Close CronTab
 echo 0 > io
-echo "Crontab自动化抓取汇率任务已关闭" | tee -a log/amatsu.log
+echo "Crontab自动化抓取汇率任务已关闭: $(date)" | tee -a log/amatsu.log
 echo "================================================================================" >> log/amatsu.log
