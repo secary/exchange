0d692cc95ecb49c60e3d9c039a443a1cc74deeac
secary <secary2010@outlook.com>
2025-03-15 18:02:30 +1030

    refactored io shells

---
+++ i.sh
@@ -0,0 +1,5 @@
+# !/bin/zsh
+# Open CronTab 
+echo 1 > io
+echo "Crontab自动化抓取汇率任务已开启" | tee -a log/amatsu.log
+echo "================================================================================" >> log/amatsu.log
--- run_amatsukaze.sh
+++ run_amatsukaze.sh
@@ -1,36 +1,24 @@
-#!/bin/zsh
-
-# 设置 PATH
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/mt/.local/bin:/mnt/c/Windows/System32/WindowsPowerShell/v1.0"
 
 # 定义日志文件和分割线
 LOG_FILE="/home/mt/root/exchange/log/amatsu.log"
-SEPARATOR1="----------------------------------------"
-SEPARATOR2="========================================"
+SEPARATOR1="--------------------------------------------------------------------------------"
+SEPARATOR2="================================================================================"
 
-# 获取命令行参数，默认为 1
-RUN_PYTHON_SCRIPT=${1:-1}
+# 添加分割线并记录脚本执行时间
+echo "$SEPARATOR2" >> "$LOG_FILE"
+echo "开始抓取汇率: $(date)" >> "$LOG_FILE"
 
-# 添加分割线和执行时间
-echo "$SEPARATOR2" | tee -a "$LOG_FILE"
-echo "开启汇率抓取: $(date)" | tee -a "$LOG_FILE"
-
-# 添加分割线并记录 Python 脚本执行
-echo "$SEPARATOR1" | tee -a "$LOG_FILE"
-
-# 如果开关为 1，则执行 Python 脚本
-if [ "$RUN_PYTHON_SCRIPT" -eq 1 ]; then
-  echo "执行 Amatsukaze.py: $(date)" | tee -a "$LOG_FILE"
-  # 使用 tee 来同时输出到终端和日志文件
-  /usr/bin/python3 /home/mt/root/exchange/Amatsukaze.py | tee -a "$LOG_FILE"
-else
-  echo "关闭汇率抓取自动化: $(date)" | tee -a "$LOG_FILE"
-fi
+echo "$SEPARATOR1" >> "$LOG_FILE"
+echo "执行 Amatsukaze.py: $(date)" >> "$LOG_FILE"
+# 执行python脚本
+/usr/bin/python3 /home/mt/root/exchange/Amatsukaze.py >> "$LOG_FILE" 2>&1
 
 # 添加分割线并记录结束时间
-echo "$SEPARATOR1" | tee -a "$LOG_FILE"
-echo "执行结束: $(date)" | tee -a "$LOG_FILE"
-echo "$SEPARATOR2" | tee -a "$LOG_FILE"
+echo "$SEPARATOR1" >> "$LOG_FILE"
+echo "脚本执行结束: $(date)" >> "$LOG_FILE"
+echo "$SEPARATOR2" >> "$LOG_FILE"
 
 # 确保脚本退出
 exit 0
+
